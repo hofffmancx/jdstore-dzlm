e718012fae3fe0941eb5b23bfad9d3d69119682e
duzilong <122517637@qq.com>
2017-06-10 09:02:59 +0800

    carts show insert

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,4 +9,16 @@ class CartsController < ApplicationController
   def checkout
     @order = Order.new
   end
+
+  def show
+    @order = Order.find_by_token(params[:id])   
+    @product_lists = @order.product_lists 
+  end 
+
+  private
+
+   def order_params
+     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address, :is_paid, :payment_method)
+   end
+
 end
